eff2e3db60013cc7cc43d80006b26cfd000286b5
Vladimir Odintcev <vovaodintcev@gmail.com>
2022-09-22 12:27:18 +0500

    add file

---
+++ src/main/java/FormDate.java
@@ -0,0 +1,5 @@
+public class FormDate {
+    public int day; // Поле дня
+    public int month; // Поле месяца
+    public int year; // Поле года
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+
+    public FormDate birthday;
+    public String name; // поле для имени
+    public String passport; // Поле для паспорта
+    public String patronymic; // поле для отчества
+    public String phone; // поле для телефона
+    public String surname; // поле для фамилии
+    public Boolean subscription;// поле для согласия или нет об скидках по смс
+}
